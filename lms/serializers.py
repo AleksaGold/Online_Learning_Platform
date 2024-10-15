14d29784a6f632b8e182ca855673a39523095ffd
Alexandra Goldobina <goldobina.s@gmail.com>
2024-10-15 20:42:01 +0500

    GRUD для модели Lesson

--- lms/serializers.py
+++ lms/serializers.py
@@ -5,6 +5,7 @@ from lms.models import Course, Lesson
 
 class CourseSerializer(ModelSerializer):
     """Сериализатор для модели Course"""
+
     class Meta:
         model = Course
         fields = "__all__"
@@ -12,5 +13,7 @@ class CourseSerializer(ModelSerializer):
 
 class LessonSerializer(ModelSerializer):
     """Сериализатор для модели Lesson"""
-    model = Lesson
-    fields = "__all__"
+
+    class Meta:
+        model = Lesson
+        fields = "__all__"
