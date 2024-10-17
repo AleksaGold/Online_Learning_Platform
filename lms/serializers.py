425d386d519965edc05230ab988d577ce9a98e6e
Alexandra Goldobina <goldobina.s@gmail.com>
2024-10-17 14:41:31 +0500

    Задание 1

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from lms.models import Course, Lesson
 
@@ -9,6 +9,18 @@ class CourseSerializer(ModelSerializer):
     class Meta:
         model = Course
         fields = "__all__"
+
+
+class CourseDetailSerializer(ModelSerializer):
+    """Сериализатор для поля вывода количества уроков Курса"""
+    count_lessons = SerializerMethodField()
+
+    def get_count_lessons(self, course):
+        return Lesson.objects.filter(course=course).count()
+
+    class Meta:
+        model = Course
+        fields = ("id", "name", "description", "preview", "count_lessons",)
 
 
 class LessonSerializer(ModelSerializer):
